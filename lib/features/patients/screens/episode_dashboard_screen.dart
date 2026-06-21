6ef4f4a26afeeed35c19cdbb0517dd55ed7bcfbd
jeanclaudebrucher <jc.brucher@lapose.net>
2026-06-21 18:05:48 +0200

    suppression code mort

--- lib/features/patients/screens/episode_dashboard_screen.dart
+++ lib/features/patients/screens/episode_dashboard_screen.dart
@@ -149,26 +149,3 @@ class _DashboardCard extends StatelessWidget {
     );
   }
 }
-
-class _DisabledDashboardCard extends StatelessWidget {
-  final IconData icon;
-  final String title;
-  final String subtitle;
-
-  const _DisabledDashboardCard({
-    required this.icon,
-    required this.title,
-    required this.subtitle,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        leading: Icon(icon),
-        title: Text(title),
-        subtitle: Text(subtitle),
-      ),
-    );
-  }
-}
--- lib/features/patients/screens/episode_results_screen.dart
+++ lib/features/patients/screens/episode_results_screen.dart
@@ -52,7 +52,7 @@ class EpisodeResultsScreen extends StatelessWidget {
           return ListView.separated(
             padding: const EdgeInsets.all(24),
             itemCount: results.length,
-            separatorBuilder: (_, __) => const SizedBox(height: 8),
+            separatorBuilder: (_, _) => const SizedBox(height: 8),
             itemBuilder: (context, index) {
               return _EpisodeResultTile(
                 result: results[index],
